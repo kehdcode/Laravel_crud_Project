39a18c970b1961543428439a09232cbdd5455803
github_kehdcode <kehindedawood@gmail.com.com>
2018-07-29 18:34:58 +0100

    update the pages

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Laravel CRUD Sytem</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/app.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+@include('/layout/nav')
+@yield('content')
+
+
+
+ <script type="text/javascript" src="/js/app.js"></script>  
+</body>
+</html>
--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Laravel CRUD Sytem</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/app.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-@yield('content')
+
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -35,6 +24,3 @@
   </div>
 </nav>
 
- <script type="text/javascript" src="/js/app.js"></script>  
-</body>
-</html>
