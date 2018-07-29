16d93a9f860e3487343dfd492997f6d9fe5ab6d7
github_kehdcode <kehindedawood@gmail.com.com>
2018-07-29 20:09:37 +0100

    craete forms

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,33 @@
 @extends('layout.app')
 
 @section('content')
-<a href="#" class="btn btn-default">Default</a>
-<a href="#" class="btn btn-default">Default</a>
-<a href="#" class="btn btn-primary">Primary</a>
-<a href="#" class="btn btn-success">Success</a>
-<a href="#" class="btn btn-info">Info</a>
-<a href="#" class="btn btn-warning">Warning</a>
-<a href="#" class="btn btn-danger">Danger</a>
-<a href="#" class="btn btn-link">Link</a>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+        <form class="form-horizontal">
+  <fieldset>
+    <legend>Laravel CRUD System</legend>
+    <div class="form-group">
+      <label for="inputEmail" class="col-lg-2 control-label">Title</label>
+      <div class="col-lg-10">
+        <input type="text" class="form-control" name="title" id="inputEmail" placeholder="Title">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="inputPassword" class="col-lg-2 control-label">Description</label>
+      <div class="col-lg-10">
+        <input type="password" class="form-control" name="description" id="inputPassword" placeholder="Description">
+    </div>
+    <div class="form-group">
+      <div class="col-lg-10 col-lg-offset-2">
+        <button type="reset" class="btn btn-default">Cancel</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+    </div>
+  </fieldset>
+</form>
+        </div>
+
+    </div>
+</div>
 @endsection
